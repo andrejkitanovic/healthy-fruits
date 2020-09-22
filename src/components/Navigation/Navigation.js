5fbe8ab2325b8d56b4d22cc88dd98d02f10c3130
unknown <kitanovicandrej213@gmail.com>
2020-09-22 16:07:29 +0200

    mobile responsive

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ const navigation = (props) => (
   <div className="Navigation">
     <img src={Logo} alt="Logo" />
     <div className="links">
-      <NavLink to="/">Pocetna</NavLink>
+      <NavLink to="/" exact>Pocetna</NavLink>
       <NavLink to="/o-nama">O nama</NavLink>
       <NavLink to="/voce">Voce</NavLink>
       <NavLink to="/povrce">Povrce</NavLink>
--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -3,17 +3,24 @@ import './SideMenu.scss'
 import { NavLink } from 'react-router-dom'
 import Logo from '../../assets/images/logo.png'
 
+import {MdMail,MdPhone , MdLocationOn} from 'react-icons/md'
+
 const sideMenu = (props) => (
   <div className={"SideMenu" + (props.active ? " active" : "")}>
     <img src={Logo} alt="Logo" />
     <div className="links">
-      <NavLink to="/">Pocetna</NavLink>
+      <NavLink to="/" exact>Pocetna</NavLink>
       <NavLink to="/o-nama">O nama</NavLink>
       <NavLink to="/voce">Voce</NavLink>
       <NavLink to="/povrce">Povrce</NavLink>
       <NavLink to="/proizvodi">Proizvodi</NavLink>
       <NavLink to="/partneri">Nasi partneri</NavLink>
     </div>
+    <div className="kontakt">
+        <a href="mailto:info@email.com"><MdMail /> info@email.com</a>
+        <a href="tel:+38169333555"><MdPhone /> +381 69-333-555</a>
+        <p><MdLocationOn /> Ul. Stanoje B. 38</p>
+      </div>
   </div>
 )
 
