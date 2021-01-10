12b9a4c9f4e32d0f9906d567a34bd8c25a06ff26
andrejkitanovic <kitanovicandrej213@gmail.com>
2021-01-10 22:27:57 +0100

    finished

--- src/components/Jabuke/ProcesProizvodnje/ProcesProizvodnje.js
+++ src/components/Jabuke/ProcesProizvodnje/ProcesProizvodnje.js
@@ -12,9 +12,9 @@ const ProcesProizvodnje = (props) => {
       <div className="flex">
         <img src={ZlatniDelises} alt="Zlatni delises" />
         <p>
-          Äpple företagen som vi samarbetar med följer noga de senaste
-          utvecklingarna och förbättras hela tiden med nya system och kyl
-          förråd. Äpple fälten är utrustade med antifrost system,
+          Äppelföretagen som vi samarbetar med följer noga de senaste
+          utvecklingarna och förbättras kontinuerligt med nya system och kyl
+          anläggningar. Äppelfälten är utrustade med antifrost system,
           bevattningssystem och skydd mot hagel.
         </p>
       </div>
@@ -22,9 +22,9 @@ const ProcesProizvodnje = (props) => {
       <div className="flex">
         <p>
           Allt som görs är noga utfört inom GLOBAL GAPS villkor, vilket ger en
-          kvalitet som går att exportera över hela världen. Kyl förråden är
+          kvalitet som går att exportera över hela världen. Kyl anläggningar är
           utrustade med Ultra Low Oxygen och har en kapacitet på 20000 ton,
-          detta möjliggör att hela äpple skörden kan förvaras i upp till 300
+          detta möjliggör att hela äppelskörden kan förvaras i upp till 300
           dagar. I kylrummen är även MafRoda system installerat som sorterar
           äpplena efter vikt, färg och diameter.
         </p>
